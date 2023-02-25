4f75367ecee9b8dad41a1ccf16eb020698c909a6
Katarina Brayner <katarinabrayner@gmail.com>
2023-02-25 12:29:25 -0300

    Update category-table layout

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,22 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-import { CategoryTableComponent } from './category-table/category-table.component';
 import { CategoriesRoutingModule } from './categories-routing.module';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryTableComponent } from './category-table/category-table.component';
+
+
 
 @NgModule({
   declarations: [
-    CategoryTableComponent
+    CategoryTableComponent,
+    CategoryListComponent
   ],
   imports: [
     CommonModule,
     MatTableModule,
     MatButtonModule,
     MatIconModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CategoriesModule { }
