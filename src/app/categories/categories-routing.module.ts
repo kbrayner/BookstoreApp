9e450087369c8e7f761acacd79b59e7e8062c53e
Katarina Brayner <katarinabrayner@gmail.com>
2023-02-26 19:20:19 -0300

    Add routes for create and update

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { CategoryCreateUpdateComponent } from './category-create-update/category-create-update.component';
 
 const routes: Routes = [
-  {path: '', component: CategoryListComponent}
+  {path: '', component: CategoryListComponent},
+  {path: 'create', component: CategoryCreateUpdateComponent},
+  {path: ':id', component: CategoryCreateUpdateComponent}
 ];
 
 @NgModule({
